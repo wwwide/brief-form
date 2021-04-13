aa8fb670666fa6f2f5e73a79f2b16593d79f2ac1
Andrey <andrey.barkanov@gmail.com>
2021-04-13 05:43:33 +0300

    Export RegisteredField

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 export * from './components';
 export * from './hooks';
-export { FormInputProps, FormFieldProps, FormValuesShape, FormErrorsShape } from './types';
+export { FormInputProps, FormFieldProps, FormValuesShape, FormErrorsShape, RegisteredField } from './types';
