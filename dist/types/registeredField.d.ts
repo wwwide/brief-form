36ff8098566cb3787af993a8fff691997f8b9e6f
Andrey <andrey.barkanov@gmail.com>
2021-05-13 17:05:46 +0300

    Update RegisteredField definition

--- dist/types/registeredField.d.ts
+++ dist/types/registeredField.d.ts
@@ -1,5 +1,7 @@
+import * as React from 'react';
 export declare type RegisteredField = {
     name: string;
+    ref: React.RefObject<any>;
     required: boolean;
     validator?: (v: any, f: any) => string | undefined;
 };
--- src/types/registeredField.ts
+++ src/types/registeredField.ts
@@ -1,5 +1,8 @@
+import * as React from 'react';
+
 export type RegisteredField = {
   name: string;
+  ref: React.RefObject<any>;
   required: boolean;
   validator?: (v: any, f: any) => string | undefined;
 };
