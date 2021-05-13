c37148ffa1c9eff432e1b791944709888097f88a
Andrey <andrey.barkanov@gmail.com>
2021-05-13 16:43:14 +0300

    Export form context

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './components';
 export * from './hooks';
 export { FormInputProps, FormFieldProps, FormValuesShape, FormErrorsShape, RegisteredField } from './types';
+export * from './context';
+
