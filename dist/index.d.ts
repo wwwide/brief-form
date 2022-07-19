fbf12f3feb548d7048dcc1600fc03c3e7ebf83fd
Andrey Barkanov <andrey.barkanov@gmail.com>
2022-07-19 20:32:18 +0300

    Export FormFieldProps

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 export * from './components';
 export * from './hooks';
-export { FormInputProps, FormErrorsShape, RegisteredField, FormConfig } from './types';
+export { FormInputProps, FormFieldProps, FormErrorsShape, RegisteredField, FormConfig } from './types';
 export * from './context';
