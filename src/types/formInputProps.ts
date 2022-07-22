8ad0a471b84ae372824ddc6f93b5fda011972b2c
Andrey Barkanov <andrey.barkanov@gmail.com>
2022-07-22 13:19:16 +0300

    Make form input opts required

--- src/types/formInputProps.ts
+++ src/types/formInputProps.ts
@@ -2,7 +2,7 @@
  * Props interface for input component which should be passed and rendered inside form field.
  */
 export interface FormInputProps<V, P> {
-  opts?: P
+  opts: P
   value: V
   onChange: (value: V, error?: string) => void
   error?: string
